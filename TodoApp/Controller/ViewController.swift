48bfe0616a2b029e4fb2e6ce05415c6bad1fbf14
Hongsik Choi <playhong16@gmail.com>
2023-08-07 15:52:01 +0900

    초기화

---
+++ TodoApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by playhong on 2023/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoApp/Model/Todo.swift
@@ -0,0 +1,12 @@
+//
+//  Todo.swift
+//  TodoApp
+//
+//  Created by playhong on 2023/08/07.
+//
+
+import Foundation
+
+struct Todo {
+    var title: String
+}
